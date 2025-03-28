4d3011fe33d51c7fa21e94f33ec9f529831f6da5
FHMM14 <fhmm.1999@gmail.com>
2025-03-28 15:04:53 -0400

    Delete Progrannacion Orientada a Objeto directory

--- Progrannacion Orientada a Objeto/Vector de cadena de caracteres.py
+++
@@ -1,7 +0,0 @@
-#Crear un vector de 5 elementos de cadenas de caracteres, inicializa el vector con datos leídos por el teclado. 
-#Copia los elementos del vector en otro vector pero en orden inverso, y muéstralo por la pantalla.
-vector_cadenas = [input(f"Ingresa la cadena {i+1}: ") for i in range(5)]
-vector_inverso = vector_cadenas[::-1]
-print("Vector en orden inverso:")
-for cadena in vector_inverso:
- print(cadena)
--- Progrannacion Orientada a Objeto/Vector de numeros enteros.py
+++
@@ -1,9 +0,0 @@
-#Realizar un programa que defina un vector llamado "vector_numeros" de 10 enteros, 
-#a continuación lo inicialice con valores aleatorios (del 1 al 10) 
-#y posteriormente muestre en pantalla cada elemento del vector junto con su cuadrado y su cubo.
-import random
-vector_numeros = [random.randint(1, 10) for _ in range(10)]
-for numero in vector_numeros:
- cuadrado = numero ** 2
- cubo = numero ** 3
- print(f"Número: {numero}, Cuadrado: {cuadrado}, Cubo: {cubo}")
